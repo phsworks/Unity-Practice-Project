019016bc4256b464aaf3acaadcff91928ae5a86d
Piet-Hein Schouten <phsdevelopment@MacBook-Pro-van-Piet-Hein.local>
2025-07-07 14:43:39 +0200

    game behaviour logic with win and lose conditions added

---
+++ Assets/Scripts/Game Behaviour.cs
@@ -0,0 +1,79 @@
+
+using UnityEngine;
+using TMPro;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+
+public class GameBehaviour : MonoBehaviour
+{
+
+
+
+    public int MaxItems = 4;
+    public TMP_Text HealthText;
+    public TMP_Text ItemText;
+    public TMP_Text ProgressText;
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ItemText.text += _itemsCollected;
+        HealthText.text += _playerHP;
+
+    }
+
+    public Button WinButton;
+    private int _itemsCollected = 0;
+    public int Items
+    {
+        get
+        { return _itemsCollected; }
+        set
+        {
+            _itemsCollected = value;
+
+            ItemText.text = "Items: " + Items;
+
+            if (_itemsCollected >= MaxItems)
+            {
+                ProgressText.text = "You've found all the items!";
+
+                WinButton.gameObject.SetActive(true);
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                ProgressText.text = "Item found, only " + (MaxItems - _itemsCollected) + " more!";
+            }
+        }
+    }
+
+    private int _playerHP;
+    public int HP
+    {
+        get { return _playerHP; }
+        set
+        {
+            _playerHP = value;
+
+            HealthText.text = "Health: " + HP;
+        }
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/ItemBehaviour.cs
+++ Assets/Scripts/ItemBehaviour.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class ItemBehaviour : MonoBehaviour
 {
+    public GameBehaviour GameManager;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        GameManager = GameObject.Find("Game Manager").GetComponent<GameBehaviour>();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -13,6 +16,7 @@ public class ItemBehaviour : MonoBehaviour
         if (collision.gameObject.name == "Player")
         {
             Destroy(this.transform.gameObject);
+            GameManager.Items += 1;
         }
   }
 
